ed6a683f585a1e74378939e795c51f252963ec7e
Ishraf Zainuddin <ishrafzainuddin@Ishrafs-MacBook-Pro.local>
2026-03-27 02:25:09 +0800

    Setup Playwright web ui automation

---
+++ utils/test-data.ts
@@ -0,0 +1,10 @@
+// Test Data
+
+// URL
+export const url = 'https://www.saucedemo.com/';
+
+// User credentials
+export const user = {
+    username: 'standard_user',
+    password: 'secret_sauce',
+};
