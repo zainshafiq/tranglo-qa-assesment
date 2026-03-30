46f3e442f352ddaab016c9f9f1aba48c0b797205
Ishraf Zainuddin <ishrafzainuddin@Ishrafs-MacBook-Pro.local>
2026-03-30 12:21:20 +0800

    create post and captured id from response to use in next steps

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,10 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html', { outputFile: 'index.html' }],
-    ['list'],
-  ],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,18 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, APIRequestContext } from '@playwright/test';
 
 test('API create post', async ({ request }) => {
-    const response = await request.get('https://jsonplaceholder.typicode.com/');
-    expect(response.status()).toBe(200);
+    const postId = await createPost(request);
+    console.log('\nPassed Post ID:', postId, '\n');
+});
 
-    const postResponse = await request.post('https://jsonplaceholder.typicode.com/posts', {
+async function createPost(request: APIRequestContext) {
+    const firstPost = await request.post('https://jsonplaceholder.typicode.com/posts', {
         data: {
             title: 'My first post',
             body: 'This is the content of my first post.',
             userId: 1,
         },
     });
-    expect(postResponse.status()).toBe(201);
 
-    const responseBody= await postResponse.json();
-    console.log('Created Post:', responseBody);
-});
+    // Assert that the status code is 201 (Created) and log the response body
+    expect(firstPost.status()).toBe(201);
+    const responseBody = await firstPost.json();
+    console.log('\nCreate Post Response:', responseBody);
+    
+    // Capture the id from the response to use in all subsequent steps
+    const postId = responseBody.id;
+    // console.log('Created Post ID:', postId);
+
+    return postId;
+}
